2d1b85eda06f48d57642ed8f0ce82ab40051358c
Konstantin Pankratov PC <pankratovkm@gmail.com>
2024-06-15 16:31:47 +0200

    Added diffrerent sizes

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -4,15 +4,26 @@ import {
   AvatarFallback,
   AvatarImage } from "@/components/ui/avatar"
 import Link from "next/link";
-import { useState } from "react";
+import React, { useState } from "react";
 import { Bell } from "lucide-react";
+import { cn } from "@/lib/utils";
 
-const Header = function () {
+interface HeaderProps {
+  size?: string
+}
+
+const Header: React.FC <HeaderProps> = ({ size }) => {
   const [loggedIn, setLoggedIn] = useState<Boolean>(true)
 
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex items-center justify-between py-4">
+      <div className={cn(
+        "flex items-center justify-between py-4",
+        {
+          "container": size !== 'full',
+          "px-6": size === 'full',
+        }
+      )}>
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5 font-bold text-xl">
             EDUPLA
