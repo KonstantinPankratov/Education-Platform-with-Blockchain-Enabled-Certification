843a09655e705b7c26f9e0b19e1c5a7147d73452
Konstantin Pankratov PC <pankratovkm@gmail.com>
2024-06-15 16:32:26 +0200

    New components

--- src/components/ui/codeEditor.tsx
+++ src/components/ui/codeEditor.tsx
@@ -2,25 +2,17 @@ import React from 'react';
 import Editor, { OnChange } from '@monaco-editor/react';
 
 const CodeEditor: React.FC = () => {
-  const [code, setCode] = React.useState<string>(`
-    const numbers = [45, 4, 9, 16, 25];
-
-    let txt = "";
-    numbers.forEach(myFunction);
-
-    function myFunction(value) {
-    // Complete this function ...
-    }`);
+  const [code, setCode] = React.useState<string>(`const numbers = [45, 4, 9, 16, 25];\n\nlet txt = "";\n\nnumbers.forEach(myFunction);\n\nfunction myFunction(value) {\n  // Complete this function ...\n}`);
 
   const handleEditorChange: OnChange = (value) => {
     setCode(value || '');
   };
 
   return (
-    <div className='rounded-lg overflow-hidden'>
+    <div className='rounded-md overflow-hidden h-full mb-5'>
         <Editor
             className="h-full w-full"
-            language="typescript"
+            language="javascript"
             theme="vs-dark"
             value={code}
             onChange={handleEditorChange}/>
