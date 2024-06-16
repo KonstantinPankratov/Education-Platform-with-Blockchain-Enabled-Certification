dbda3cd070f1ea568b193ac223a0a1352dac9c79
Konstantin Pankratov PC <pankratovkm@gmail.com>
2024-06-16 16:34:33 +0200

    Added custom tooptip for recharts

---
+++ src/components/rechart/TwTooltip.tsx
@@ -0,0 +1,28 @@
+"use client";
+import React from "react";
+import { TooltipProps } from "recharts";
+
+interface Payload {
+  value: number;
+}
+
+interface CustomTooltipProps extends TooltipProps<number, string> {
+  payload?: Payload[];
+  label?: string;
+  desc?: string;
+}
+
+const CustomTooltip: React.FC<CustomTooltipProps> = ({ active, payload, label, desc }) => {
+  if (active && payload && payload.length) {
+    return (
+      <div className="p-3 rounded border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+        <p className="font-semibold text-neutral-50">{label}</p>
+        <p className="text-sm">{`${desc}: ${payload[0].value}`}</p>
+      </div>
+    );
+  }
+
+  return null;
+};
+
+export default CustomTooltip;
