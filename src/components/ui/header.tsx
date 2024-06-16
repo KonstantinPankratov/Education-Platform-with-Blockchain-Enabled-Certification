e678c15a540741dd4bf18a42bab153c1cf816370
Konstantin Pankratov PC <pankratovkm@gmail.com>
2024-06-16 16:50:26 +0200

    Added My Account dropdown

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,11 +1,20 @@
+import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import {
   Avatar,
   AvatarFallback,
   AvatarImage } from "@/components/ui/avatar"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu"
 import Link from "next/link";
-import React, { useState } from "react";
-import { Bell } from "lucide-react";
+import { Bell, LogOut, Scroll, User } from "lucide-react";
 import { cn } from "@/lib/utils";
 
 interface HeaderProps {
@@ -47,10 +56,33 @@ const Header: React.FC <HeaderProps> = ({ size }) => {
           <Button variant="ghost" size="icon">
             <Bell/>
           </Button>
-          <Avatar>
-            <AvatarImage src="https://avatars.githubusercontent.com/u/30325297?v=4" alt="@KonstantinPankratov" />
-            <AvatarFallback>KP</AvatarFallback>
-          </Avatar>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild className="cursor-pointer hover:opacity-70 transition-opacity">
+              <Avatar>
+                <AvatarImage src="https://avatars.githubusercontent.com/u/30325297?v=4" alt="@KonstantinPankratov" />
+                <AvatarFallback>KP</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56" align="end" sideOffset={10}>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+              <DropdownMenuItem>
+                  <User className="mr-2 h-4 w-4" />
+                  <span>Profile</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Scroll className="mr-2 h-4 w-4" />
+                  <span>Courses</span>
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <LogOut className="mr-2 h-4 w-4" />
+                <span>Log out</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </nav> }
       </div>
     </header>
