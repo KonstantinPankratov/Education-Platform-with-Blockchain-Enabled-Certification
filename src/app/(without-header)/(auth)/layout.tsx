426dec3a1e592791df63695bc4e482747560e080
Konstantin Pankratov PC <pankratovkm@gmail.com>
2024-06-19 14:15:05 +0200

    Ehanced responsiveness

--- src/app/(without-header)/(auth)/layout.tsx
+++ src/app/(without-header)/(auth)/layout.tsx
@@ -1,21 +1,23 @@
+import Link from "next/link";
+
 export default function SignInLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div className="relative overflow-hidden flex items-center justify-center w-screen h-screen p-10">
-      <div className="border rounded-2xl overflow-hidden grid grid-cols-2 flex-grow h-full">
-        <div className="lg:flex flex-col h-full bg-neutral-900 p-10">
-          <div className="text-lg font-bold">EDUPLA</div>
-          <div className="mt-auto">
+    <div className="relative overflow-hidden flex min-h-screen p-3 md:p-10">
+      <div className="border rounded-2xl overflow-y-auto grid grid-cols-1 md:grid-cols-2 flex-grow m-auto max-w-screen-2xl">
+        <div className="flex flex-col h-full text-center md:text-left border-b-2 md:bg-neutral-900 p-5 md:p-10">
+          <Link href='/' className="text-lg font-bold">EDUPLA</Link>
+          <div className="mt-auto hidden md:block">
             <blockquote className="space-y-2">
               <p className="text-lg text-neutral-50">“Chtělo se mě experimentovat, udělat něco neobvyklého, ale zároveň užitečného.”</p>
               <footer className="text-sm text-neutral-400">Author EDUPLA</footer>
             </blockquote>
           </div>
         </div>
-        <div className="lg:flex flex-col h-full p-10 justify-center items-center">
+        <div className="flex flex-col h-full p-5 md:p-10 justify-center items-center">
           {children}
         </div>
       </div>
