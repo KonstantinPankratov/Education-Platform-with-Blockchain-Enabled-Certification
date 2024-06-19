7ded0aeb852f383e4e417c3ee56346b814b4edf9
Konstantin Pankratov PC <pankratovkm@gmail.com>
2024-06-19 17:17:51 +0200

    Added next auth

---
+++ src/auth.ts
@@ -0,0 +1,33 @@
+import NextAuth from "next-auth"
+import GitHub from "next-auth/providers/github"
+import Credentials from "next-auth/providers/credentials"
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        let user = null
+ 
+        // logic to salt and hash password
+        const pwHash = saltAndHashPassword(credentials.password)
+ 
+        // logic to verify if user exists
+        user = await getUserFromDb(credentials.email, pwHash)
+ 
+        if (!user) {
+          // No user found, so this is their first attempt to login
+          // meaning this is also the place you could do registration
+          throw new Error("User not found.")
+        }
+ 
+        // return user object with the their profile data
+        return user
+      },
+    }),
+    GitHub
+  ],
+})
