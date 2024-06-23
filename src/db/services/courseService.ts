bfa31a4d424a5b9d439e598a2b0ca8a18f66a808
Konstantin Pankratov PC <pankratovkm@gmail.com>
2024-06-23 11:08:05 +0200

    Added course get services

---
+++ src/db/services/courseService.ts
@@ -0,0 +1,55 @@
+import dbConnect from "../dbConnect"
+import Course, { ICourse } from "../models/Course"
+
+interface PopulateOption {
+  path: string
+  select?: string
+  match?: any
+  options?: any
+}
+
+interface GetCourseOptions {
+  populate?: PopulateOption[]
+  sort?: any
+}
+
+export async function getCourses(
+  options: GetCourseOptions = {}
+): Promise<ICourse[]> {
+  await dbConnect()
+
+  let query = Course.find({})
+
+  if (options.sort) {
+    query = query.sort(options.sort)
+  }
+
+  if (options.populate) {
+    options.populate.forEach((populateOption) => {
+      query = query.populate(populateOption)
+    })
+  }
+  
+  return await query.exec()
+}
+
+export async function getCourseBySlug(
+  slug: string,
+  options: GetCourseOptions = {}
+): Promise<ICourse | null> {
+  await dbConnect()
+
+  let query = Course.findOne({ slug: slug })
+
+  if (options.sort) {
+    query = query.sort(options.sort)
+  }
+
+  if (options.populate) {
+    options.populate.forEach((populateOption) => {
+      query = query.populate(populateOption)
+    })
+  }
+
+  return await query.exec()
+}
