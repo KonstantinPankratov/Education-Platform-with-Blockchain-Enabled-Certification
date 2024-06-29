9d3a7da38d3d38876b1d840ce93aeac95cc8e1f5
Konstantin Pankratov PC <pankratovkm@gmail.com>
2024-06-29 18:45:06 +0200

    Fixed github sign in

--- src/components/forms/SiginWithGithub.tsx
+++ src/components/forms/SiginWithGithub.tsx
@@ -1,18 +1,13 @@
+"use client"
+
 import { Button } from "@/components/ui/button"
 import { Github } from "lucide-react"
 import { signIn } from "next-auth/react"
  
 export function SignInWithGithub() {
   return (
-    <form
-      action={async () => {
-        "use server"
-        await signIn("github")
-      }}
-    >
-    <Button variant="outline" size="lg" className="w-full" type="submit">
-        <Github size={20} className="mr-3"/> Github
+    <Button variant="outline" size="lg" className="w-full" type="submit" onClick={() => signIn('github')}>
+      <Github size={20} className="mr-3"/> Github
     </Button>
-    </form>
   )
 } 
