337aff19d8c40a7f1053253552800d6ae5362dbd
Konstantin Pankratov PC <pankratovkm@gmail.com>
2024-08-06 16:52:53 +0200

    Added shadcn chart

--- src/components/profile/card-chart.tsx
+++ src/components/profile/card-chart.tsx
@@ -1,8 +1,16 @@
 "use client"
 
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
-import { LineChart, Line, XAxis, Tooltip } from 'recharts'
-import CustomTooltip from "@/components/rechart/TwTooltip"
+import { Line, LineChart, XAxis } from "recharts"
+import { ChartConfig, ChartContainer } from "@/components/ui/chart"
+import { ChartTooltip, ChartTooltipContent } from "@/components/ui/chart"
+
+const chartConfig = {
+  quantity: {
+    label: "Completed exercises",
+    color: "#ffffff",
+  }
+} satisfies ChartConfig
 
 interface CardChartProps {
   data: any[],
@@ -17,11 +25,16 @@ const CardChart = ({ data, desc }: CardChartProps) => {
         <CardDescription>+ 2 modules this week</CardDescription>
       </CardHeader>
       <CardContent>
-        <LineChart width={300} height={120} data={data}>
-          <Tooltip content={<CustomTooltip desc={desc}/>}/>
-          <XAxis dataKey="key" hide={true}/>
-          <Line type="monotone" dataKey="value" stroke="white" strokeWidth="3" />
-        </LineChart>
+        <ChartContainer config={chartConfig} className="w-[300px] h-[120px]">
+          <LineChart data={data}>
+            <XAxis
+              dataKey="date"
+              hide={true}
+            />
+            <ChartTooltip content={<ChartTooltipContent />} />
+            <Line dataKey="quantity" stroke="var(--color-quantity)" type="monotone" strokeWidth="3" radius={4} />
+          </LineChart>
+        </ChartContainer>
       </CardContent>
     </Card>
   )
