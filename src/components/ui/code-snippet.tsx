15f7469aed32be0ab6d987f379ea25f85fa0f343
Konstantin Pankratov PC <pankratovkm@gmail.com>
2024-08-08 18:11:52 +0200

    Added "use client"

--- src/components/ui/code-snippet.tsx
+++ src/components/ui/code-snippet.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Prism } from "react-syntax-highlighter"
 import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism"
 
@@ -8,7 +10,7 @@ const CodeSnippet = ({
 }>) => {
   return (
     <Prism language="javascript" style={vscDarkPlus} showLineNumbers className="rounded-md">
-      { children?.toString() } 
+      {children?.toString()}
     </Prism>
   )
 }
