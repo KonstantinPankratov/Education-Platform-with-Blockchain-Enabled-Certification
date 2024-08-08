4abce0bbde944eeaf3e6affdf2c597d46c7aaad1
Konstantin Pankratov PC <pankratovkm@gmail.com>
2024-08-08 18:05:10 +0200

    Added profile schema

--- src/lib/zod.ts
+++ src/lib/zod.ts
@@ -1,8 +1,9 @@
 import { z } from "zod"
+import { DATE_PLACEHOLDER, DATE_REGEX } from "./dateMask"
 
 export const SignInSchema = z.object({
-    email: z.string().email(),
-    password: z.string()
+  email: z.string().email(),
+  password: z.string()
 })
 
 export type SignInSchema = z.infer<typeof SignInSchema>
@@ -18,3 +19,16 @@ export const SignUpSchema = z.object({
 // TODO pwd complexity
 
 export type SignUpSchema = z.infer<typeof SignUpSchema>
+
+
+export const ProfileSchema = z.object({
+  name: z.string({
+    required_error: "Name is required"
+  }),
+  dateOfBirth: z.string({
+    required_error: "Date of birth is required"
+  }).regex(DATE_REGEX, { message: `Invalid date format. Use ${DATE_PLACEHOLDER}` })
+})
+
+export type ProfileSchema = z.infer<typeof ProfileSchema>
+
