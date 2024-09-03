eea74873af390cdf82a7c22c9315c68a58dd0a99
Konstantin Pankratov PC <pankratovkm@gmail.com>
2024-09-03 10:04:17 +0200

    Temp solution for types

--- src/actions/course/auth/fetch-course-by-exercise-id.ts
+++ src/actions/course/auth/fetch-course-by-exercise-id.ts
@@ -2,9 +2,11 @@
 
 import dbConnect from "@/db/dbConnect"
 import Exercise from "@/db/models/Exercise";
+import { ILecture } from "@/db/models/Lecture";
+import { IModule } from "@/db/models/Module";
 import { Types } from "mongoose";
 
-const fetchCourseByExerciseId = async (exerciseId: string): Promise<string> => {
+const fetchCourseByExerciseId = async (exerciseId: string): Promise<string | undefined> => {
   try {
     await dbConnect()
 
@@ -20,7 +22,14 @@ const fetchCourseByExerciseId = async (exerciseId: string): Promise<string> => {
       }
     })
 
-    return exercise?.lectureId.moduleId.courseId
+    if (!exercise) {
+      throw new Error('Invalid exercise ID')
+    }
+
+    const lecture = exercise.lectureId as unknown as ILecture
+    const mod = lecture.moduleId as unknown as IModule
+
+    return mod.courseId as unknown as string
   } catch (error: any) {
     console.error(error)
   }
