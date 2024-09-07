e61adf0a403b62f1694efd81d2e67cf7e4f91579
Konstantin Pankratov PC <pankratovkm@gmail.com>
2024-09-07 13:12:35 +0200

    Removed comment

--- src/components/exercise/result/loading.tsx
+++ src/components/exercise/result/loading.tsx
@@ -7,7 +7,7 @@ const LoadingResult = () => {
   return (
     <>
       <Skeleton className="h-6 w-2/5" />
-      <ul className={cn('pl-4 flex flex-col gap-2 text-neutral-700', styles.stdout)}> {/* TODO */}
+      <ul className={cn('pl-4 flex flex-col gap-2 text-neutral-700', styles.stdout)}>
         <li className='pl-2 text-sm'><Skeleton className="h-4 w-3/4 bg-neutral-800" /></li>
         <li className='pl-2 text-sm'><Skeleton className="h-4 w-1/5" /></li>
         <li className='pl-2 text-sm'><Skeleton className="h-4 w-1/2" /></li>
