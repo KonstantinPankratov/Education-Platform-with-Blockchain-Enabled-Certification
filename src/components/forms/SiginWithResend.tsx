8378eada3bbc76a4d254fde9f2b9aade32b5434c
Konstantin Pankratov PC <pankratovkm@gmail.com>
2024-11-16 09:42:45 +0100

    Fix: disabling button while sending an email

--- src/components/forms/SiginWithResend.tsx
+++ src/components/forms/SiginWithResend.tsx
@@ -8,6 +8,7 @@ import { useForm } from "react-hook-form"
 import { Form, FormControl, FormField, FormItem, FormMessage } from "../ui/form"
 import { ResendFormSchema } from "@/lib/zod"
 import { toast } from "sonner"
+import { useState } from "react"
 
 export function SignInWithResend() {
   const form = useForm<ResendFormSchema>({
@@ -17,13 +18,21 @@ export function SignInWithResend() {
     },
   })
 
-  function onSubmit(values: ResendFormSchema) {
-    resendSignIn(values).then(() => {
+  const [isLoading, setLoading] = useState(false)
+
+  async function onSubmit(values: ResendFormSchema) {
+    setLoading(true)
+    try {
+      await resendSignIn(values)
       toast.success("Check your email, a sign in link has been sent to your email address.", {
         duration: 10000,
         position: "bottom-center",
       })
-    })
+    } catch (error) {
+      toast.error("An error occurred while sending the sign-in link.")
+    } finally {
+      setLoading(false)
+    }
   }
 
   return (
@@ -41,7 +50,9 @@ export function SignInWithResend() {
             </FormItem>
           )}
         />
-        <Button type="submit">Sign in with email</Button>
+        <Button type="submit" disabled={isLoading}>
+          {isLoading ? 'Sending email...' : 'Sign in with email'}
+        </Button>
       </form>
     </Form>
   )
