8a7ee6a2de3ceb7029bbc29e6a96a43f6acd08df
Konstantin Pankratov PC <pankratovkm@gmail.com>
2025-01-13 18:48:55 +0100

    Fixes #10: added tonconnect manifest to personalize ton wallet connection

--- src/app/(common-header)/tonconnect-provider.tsx
+++ src/app/(common-header)/tonconnect-provider.tsx
@@ -7,7 +7,7 @@ export default function TonConnectProvider({
 }: {
   children: React.ReactNode
 }) {
-  const manifestUrl = 'https://api.npoint.io/f3d382c8227d17891a06'; //`${process.env.NEXT_PUBLIC_SITE_HOST}/tonconnect-manifest.json`;
+  const manifestUrl = `${process.env.NEXT_PUBLIC_TON_MANIFEST_URL}`;
 
   return <TonConnectUIProvider manifestUrl={manifestUrl}>{children}</TonConnectUIProvider>
 }
